cd1f2d375528de52bedf79b698ba15ebbd339bfd
Josh <jthalappillil7@gmail.com>
2024-06-13 14:50:55 +1000

    Added tags label and content to display category of quotes. update button and label styling

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ def fetch_quote():
     data = response.json()
     quote = data["content"]
     author = data["author"]
+    tags = data["tags"]
 
     
-    return quote, author
+    return quote, author, tags
 
 def update_quote():
-    quote, author = fetch_quote()
+    quote, author, tags = fetch_quote()
+    tags_str = ', '.join(tags)
     quote_label.config(text=quote)
     author_label.config(text=f"~ {author}")
-    
+    tags_label.config(text=f"Category: {tags_str}")
 
 
 root = ttk.Window(themename="pulse")
@@ -30,12 +32,15 @@ frame.pack(padx = 30, pady = 40)
 quote_label = ttk.Label(frame, text="", font=("Helvetica", 16), wraplength = 650)
 quote_label.pack()
 
-author_label = ttk.Label(frame, text="", font=("Helvetica", 12))
+author_label = ttk.Label(frame, text="", font=("Helvetica", 12, "bold"))
 author_label.pack(pady = 10)
 
+tags_label = ttk.Label(frame, text="", font=("Helvetica", 12)) 
+tags_label.pack(pady = 10)
 
-
-ttk.Button(frame, text="Get Quote", command = update_quote).pack(pady = 20)
+style = ttk.Style()
+style.configure("TButton", font=("Helvetica", 12))
+ttk.Button(frame, text="Get Quote", style="TButton", command = update_quote).pack(pady = 20)
 root.mainloop()
 
 
